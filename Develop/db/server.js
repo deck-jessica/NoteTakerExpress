db9f719b5e5a40938cdf93d45bfa742c7cf6e3c1
Jessica D <deck.jessica@gmail.com>
2021-01-17 19:37:03 -0500

    starting to set up routes for api and html handling

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -1,12 +1,19 @@
+// set up requirements for server and to build out pages
 var express = require("express");
+var fs = require("fs");
+var path = require("path");
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = 8080;
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
