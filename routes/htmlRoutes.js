bb1dbf76a71468f9e3fd38e0705a434d43e6b178
Jessica D <deck.jessica@gmail.com>
2021-01-18 20:47:06 -0500

    finished up saving the note to db/post request, restored functionality with correct paths

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,11 +5,11 @@ var path = require("path");
 module.exports = function(app) {
 // here is what happens when a user visits a route
   app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
 
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("./public"));
 
 
 
