2a8c1ec39cca6f39d35a18aa9d0215a70849c9dc
Jessica D <deck.jessica@gmail.com>
2021-01-18 21:42:53 -0500

    finished delete route, added images and content into readme file

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # NoteTakerExpress
+
+## Description
+
+This application can be used to save notes to a server (in this case, Express) so they can be written, viewed, saved and deleted through this web application. Notes are converted to JSON for storage, and stringified to display on applciation. The front end application is housed in the public folder. The database and server functionality are in the server.js file, which accesses the routes (web and API).
+
+[Note Taker Express Live Link](https://drive.google.com/file/d/1F5nKVIAQ5wTZPbYdis90Nugyp6QNgmEs/view)
+
+![Note Taker Express Home](/public/assets/images/homepage.png)
+
+![Note Taker Express Note](/public/assets/images/notepage.png)
+
+ ## Table of Contents
+
+  *[License](#license)
+
+  *[Installation](#installation)
+
+  *[Usage](#usage)
+
+  *[Contribution](#contribution)
+
+  *[Tests](#tests)
+
+  *[Questions](#questions)
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow?style=plastic.svg)](https://opensource.org/licenses/MIT)
+
+## Installation
+
+You will need to ensure you have the following already downloaded to run the application:
+
+* [Node.js](https://nodejs.org/)
+* [Express.js](https://expressjs.com/)
+
+Once the above are downloaded, to properly use this generator, please install the following dependencies by running:
+
+`
+npm i
+npm install express
+npm install uuid
+`
+
+## Usage
+
+Once dependencies have been installed, you can run the application in the terminal with:
+
+`
+node server.js
+`
+and open using localhost. Or if accessing via web, visit the deployed link above.
+
+## Contributors
+
+This project was created with the help of my bootcamp class materials and my tutor, as well as some research done on GET, POST and DELETE requests. Also with the help of my classmates during study sessions
+
+## Testing
+
+This has not been set up for testing yet.
+
+## Questions
+
+For any questions, you can visit the [GitHub page](https://github.com/deck-jessica).
+  Or email directly to [email](mailto:deck.jessica@gmail.com).
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,17 +5,16 @@ const { v4: uuidv4 } = require('uuid');
 
 module.exports = function(app) {
     // function to write notes as strings in db.json
-    function writeToDB(notes){
-        notes = JSON.stringify(notes);
-        fs.writeFileSync("./db/db.json", notes, function(err) {
-                if (err) {
-                    return console.log(err);
-                }
+    function writeToDB(){
+        fs.writeFileSync("./db/db.json", JSON.stringify(notesData), function(err) {
+                if (err) throw err;
+                console.log('Saved!');
         });
     }
 
     // API GET Request
     app.get("/api/notes", function(req, res) {
+        // here if someone goes to the notes page, they are given the notes saved in db
       res.json(notesData);
     });
   
@@ -24,18 +23,31 @@ module.exports = function(app) {
     // API POST Request
 
     app.post("/api/notes", function(req, res) {
-   
-    notesData.push(req.body);
-
-    res.json(req.body);
+        // here is where the request is made to add a new note to db, gather info from body of request
+        var noteToAdd = req.body;
+        // got some help to add randomly generated id using uuid, add it as the id param by calling the function on it
+        noteToAdd.id = uuidv4();
+        // once the id has been added, noteToAdd can be pushed into the array notesData, stored in db
+        notesData.push(noteToAdd);
+        res.json(noteToAdd);
 
     writeToDB();
     });
 
     // API DELETE Request
     app.delete("/api/notes/:id", function(req, res) {
-
-
+        // when clicking on delete, need to access the id property to be able to identify which note is being deleted
+        var noteToDelete = req.params.id;
+        // loop through notes in notesData array, to find the one with matching id
+        for (let i = 0; i < notesData.length; i++) {
+            if (noteToDelete == notesData[i].id){
+                // splice is starting at i, the matching id, and removing ONE note here
+                notesData.splice(i, 1)
+            } 
+        }
+        // send new array without the note that just got deleted
+        res.send(notesData);
+        // rewrite the array to the db
         writeToDB();
     });
 
